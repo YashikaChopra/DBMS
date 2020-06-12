b0ebebe59324a675f2fd7505af01f998182bd2ae
YashikaChopra <66349179+YashikaChopra@users.noreply.github.com>
2020-06-12 12:47:15 +0530

    Create urls.py
    for view

---
+++ studs/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from django.conf.urls import include
+from rest_framework import routers
+from . import views
+
+router = routers.DefaultRouter()
+router.register(r'user', views.LocationViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+] 
