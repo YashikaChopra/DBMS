8f422d75191ae406e4b174f2db7d3fb5578ff18d
YashikaChopra <66349179+YashikaChopra@users.noreply.github.com>
2020-06-12 12:48:25 +0530

    Update views.py

--- studs/views.py
+++ studs/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import ClassLocationSerializer
+from .models import ClassLocation
+
+class LocationViewSet(viewsets.ModelViewSet):
+    queryset = ClassLocation.objects.all()
+    serializer_class = ClassLocationSerializer
 
 # Create your views here.
